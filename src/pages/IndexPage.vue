b81c7fc30991c63fc7fa972232e6d43cfe80acfd
levannghia <meoem2712@gmail.com>
2023-09-05 21:58:13 +0700

    Update V2

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,26 +1,78 @@
 <template>
   <q-page>
     <div class="bg-primary" id="custom-money">
-      <div class="text-h4 text-left text-white box-shadow q-pa-md">
-        10.000
+      <div class="text-white box-shadow q-pa-md row justify-between items-center">
+        <div class="text-left">
+          <span class="text-subtitle2">Số dư</span>
+          <div class="text-h5 q-mt-sm">10.000</div>
+        </div>
+        <div  class="text-right">
+          <q-btn icon="event" round color="primary">
+            <q-popup-proxy
+              @before-show="updateProxy"
+              cover
+              transition-show="scale"
+              transition-hide="scale"
+            >
+              <q-date v-model="filterDate" range mask="DD-MM-YYYY">
+                <div class="row items-center justify-end q-gutter-sm">
+                  <q-btn label="Cancel" color="primary" flat v-close-popup />
+                  <q-btn
+                    label="OK"
+                    color="primary"
+                    flat
+                    @click="save"
+                    v-close-popup
+                  />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-btn>
+          <div class="q-mt-xs">
+            <q-badge> {{ filterDate.from }} <q-icon name="arrow_right_alt" /> {{ filterDate.to }} </q-badge>
+          </div>
+        </div>
       </div>
     </div>
     <div class="q-pa-sm">
       <div class="q-pa-md row justify-center">
-        <q-input style="min-width: 10em" type="number" v-model.number="virtualListIndex" :min="0" :max="9999"
-          label="Scroll to index" input-class="text-right" outlined />
-        <q-btn class="q-ml-sm" label="Go" no-caps color="primary" @click="executeScroll" />
+        <q-input
+          style="min-width: 10em"
+          type="number"
+          v-model.number="virtualListIndex"
+          :min="0"
+          :max="9999"
+          label="Scroll to index"
+          input-class="text-right"
+          outlined
+        />
+        <q-btn
+          class="q-ml-sm"
+          label="Go"
+          no-caps
+          color="primary"
+          @click="executeScroll"
+        />
       </div>
 
       <q-separator />
 
-      <q-virtual-scroll ref="virtualListRef" style="max-height: 100vh;" component="q-list" :items="heavyList" separator
-        @virtual-scroll="onVirtualScroll" v-slot="{ item, index }">
-        <q-item :key="index" dense :class="{ 'bg-black text-white': index === virtualListIndex }">
+      <q-virtual-scroll
+        ref="virtualListRef"
+        style="max-height: 100vh"
+        component="q-list"
+        :items="heavyList"
+        separator
+        @virtual-scroll="onVirtualScroll"
+        v-slot="{ item, index }"
+      >
+        <q-item
+          :key="index"
+          dense
+          :class="{ 'bg-black text-white': index === virtualListIndex }"
+        >
           <q-item-section>
-            <q-item-label>
-              #{{ index }} - {{ item.label }}
-            </q-item-label>
+            <q-item-label> #{{ index }} - {{ item.label }} </q-item-label>
           </q-item-section>
         </q-item>
       </q-virtual-scroll>
@@ -29,44 +81,42 @@
 </template>
 
 <script setup>
-import useAuthUser from 'src/composables/useAuthUser';
-import { ref, onMounted } from 'vue'
+import useAuthUser from "src/composables/useAuthUser";
+import { ref, onMounted } from "vue";
 
-const { user } = useAuthUser()
-const maxSize = 1000
-const heavyList = []
+const { user } = useAuthUser();
+const maxSize = 1000;
+const heavyList = [];
+const filterDate = ref({ from: "08-07-2023", to: "17-09-2023" });
 
 for (let i = 0; i < maxSize; i++) {
   heavyList.push({
-    label: 'Option ' + (i + 1)
-  })
+    label: "Option " + (i + 1),
+  });
 }
 
-
-const virtualListRef = ref(null)
-const virtualListIndex = ref(50)
+const virtualListRef = ref(null);
+const virtualListIndex = ref(50);
 
 onMounted(() => {
-  virtualListRef.value.scrollTo(virtualListIndex.value)
-
-})
-
+  virtualListRef.value.scrollTo(virtualListIndex.value);
+});
 
 const onVirtualScroll = ({ index }) => {
-  virtualListIndex.value = index
+  virtualListIndex.value = index;
   console.log("test");
-}
+};
 
 const executeScroll = () => {
-  virtualListRef.value.scrollTo(virtualListIndex.value, 'start-force')
-}
-
+  virtualListRef.value.scrollTo(virtualListIndex.value, "start-force");
+};
 </script>
 
 <style>
-#custom-money{
+#custom-money {
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
 }
 </style>
