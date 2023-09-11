faeb69f342bbc7508cc1bfcb7c1d4b377bdc6822
levannghia <meoem2712@gmail.com>
2023-09-11 22:04:15 +0700

    Len

--- src/pages/ChartPage.vue
+++ src/pages/ChartPage.vue
@@ -10,48 +10,66 @@
         </q-card-section>
       </q-card-section>
     </q-card>
-
   </q-page>
 </template>
 
 <script setup>
 import { onUnmounted, onMounted, ref, watch } from "vue";
 import { useCategoryStore } from "src/stores/category-store";
+import useAuthUser from "src/composables/useAuthUser";
 import useApi from "src/composables/useApi";
 import useNotify from "src/composables/useNotify";
-import useAuthUser from "src/composables/useAuthUser"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Doughnut } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "vue-chartjs";
 // import * as chartConfig from './chartConfig.js'
-import useSupabase from 'src/boot/supabase'
+import useSupabase from "src/boot/supabase";
 
-ChartJS.register(ArcElement, Tooltip, Legend)
-const { supabase } = useSupabase()
-const categoryStore = useCategoryStore()
+ChartJS.register(ArcElement, Tooltip, Legend);
+const { supabase } = useSupabase();
+const { user } = useAuthUser();
+const categoryStore = useCategoryStore();
 const { fetchListData, removeById } = useApi();
 const { notifyError, notifySuccess } = useNotify();
+const dataCategory = ref([]);
 const data = ref({
-  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+  labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
   datasets: [
     {
-      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-      data: [40, 20, 80, 10]
-    }
-  ]
-})
+      backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
+      data: [40, 20, 80, 10],
+    },
+  ],
+});
 
 const options = ref({
   responsive: true,
-  maintainAspectRatio: false
-})
+  maintainAspectRatio: false,
+});
+const categoryIdColumn = 'categoryId'; // Thay thế bằng tên cột categoryId của bạn
+const priceColumn = 'price';
+
+const calculateTotalPriceByCategory = async () => {
+  try {
+    const { data, error } = await supabase
+      .from("notes")
+       .rpc('calculate_total_price_by_category', { categoryIdColumn, priceColumn });
+    if (error) throw error;
+    console.log("Tổng giá theo categoryId:", data);
+    return data;
+  } catch (error) {
+    notifyError(error.message);
+  }
+};
+
+// calculateTotalPriceByCategory()
 
 onMounted(() => {
-  categoryStore.bcrumb = 'Biểu đồ'
-})
+  categoryStore.bcrumb = "Biểu đồ";
+});
 
 onUnmounted(() => {
-  categoryStore.bcrumb = ''
-})
+  categoryStore.bcrumb = "";
+});
 </script>
 
 <style></style>
